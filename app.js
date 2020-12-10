ec9fd3235fe60511ecc233700141f96449417c2f
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2020-12-10 12:05:22 -0500

    added email, password feedback

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // 1. get DOM element
 const regForm = document.getElementById('registration-form')
+
 const username = document.getElementById('username')
-const usernameErr = username.nextElementSibling
+const usernameError = username.nextElementSibling
 // const usernameErr = document.getElementById("usernameErr")
+
+const email = document.getElementById('email')
+const emailError = email.nextElementSibling
+
+const password = document.getElementById('password')
+const passwordError = password.nextElementSibling
+
+const password2 = document.getElementById('password-2')
+const password2Error = password2.nextElementSibling
 
 // 2. Add event / modification
 
@@ -12,13 +22,41 @@ regForm.addEventListener('click', function (e) {
   // 2. Check if the value is empty
   if (username.value === '') {
     // 3. If empty, give user some feedback
-    usernameErr.className = 'block bg-red-500 text-white'
+    usernameError.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
+  }
+})
+
+regForm.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  //@TODO: add the validation for email
+  // 2. Check if the value in variable 'email' is empty
+  if (email.value === '') {
+    // 3. If empty, give user some feedback
+    emailError.className = 'block bg-red-500 text-white'
   } else {
     console.log('sending to server')
   }
 
-  //@TODO: add the validation for email
-
   //@TODO: add the validation for password
+  // 2. Check if the value in variable 'password' is empty
+  if (password.value === '') {
+    // 3. If empty, give user some feedback
+    passwordError.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
+  }
 
+
+
+  //@TODO: add the validation for confirm password
+  // 2. Check if the value in variable 'password2' is empty
+  if (password.value === '') {
+    // 3. If empty, give user some feedback
+    password2Error.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
+  }
 })
